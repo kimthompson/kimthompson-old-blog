b2881dcbc3d02141ac47a29f42b9809bd3b77ca0
Kim Thompson <kthompson@mpr.org>
2020-09-10 17:56:59 -0500

    basic styles, but some of my assets

---
+++ src/routes/index.svelte
@@ -0,0 +1,77 @@
+<style>
+  /*.home-container {*/
+    /*align-items: center;*/
+    /*display: flex;*/
+    /*flex: 1;*/
+    /*justify-content: center;*/
+    /*margin: 2em 0;*/
+    /*min-height: 400px;*/
+  /*}*/
+
+  /*.home {*/
+    /*flex: 1;*/
+  /*}*/
+
+  /*h1 {*/
+    /*font-weight: 700;*/
+    /*margin-bottom: 0.5em;*/
+  /*}*/
+
+  /*p {*/
+    /*font-size: 1.4em;*/
+    /*line-height: 1.5;*/
+  /*}*/
+
+  /*figure {*/
+    /*margin: 0 1em;*/
+    /*text-align: center;*/
+  /*}*/
+
+  /*figcaption {*/
+    /*font-size: .8em;*/
+    /*font-style: italic;*/
+  /*}*/
+
+  /*img {*/
+    /*width: 100%;*/
+    /*max-width: 400px;*/
+  /*}*/
+
+  /*@media (max-width: 1020px) {*/
+    /*p {*/
+      /*font-size: 1.2em;*/
+    /*}*/
+
+    /*img {*/
+      /*max-width: 300px;*/
+    /*}*/
+  /*}*/
+
+  /*@media (max-width: 800px) {*/
+    /*.home-container {*/
+      /*flex-direction: column;*/
+    /*}*/
+
+    /*.home {*/
+      /*flex: 0;*/
+      /*padding-bottom: 2em;*/
+      /*text-align: center;*/
+    /*}*/
+  /*}*/
+</style>
+
+<svelte:head>
+  <title>Kim Thompson</title>
+</svelte:head>
+
+<div class="home-container">
+  <figure>
+    <img alt='Me, being grumpy and getting caffeine' src='grump.png'>
+    <figcaption>Art by <a href="https://www.instagram.com/lady_red_art/" target="_blank">@lady_red_art</a> on Instagram</figcaption>
+  </figure>
+
+  <div class="home">
+    <h1>Kim Thompson</h1>
+    <p>Web Developer</p>
+  </div>
+</div>
